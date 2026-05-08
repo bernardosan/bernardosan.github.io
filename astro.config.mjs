8e0cab7c1a98a1e066cb355774d6d196b937395f
Bernardosan <bernardosantiagodesouza@gmail.com>
2026-05-08 17:39:21 +0100

    Deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bernardosan.github.io",
+  base: "/bernardosouza",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
